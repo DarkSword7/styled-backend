c67ad51c383619124e58db322c97f1077785fd68
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-06-08 13:26:47 +0530

    commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,29 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+      apiToken: "<vercel-api-token>",
+      appFilter: "your-app-name-on-vercel",
+      teamFilter: "your-team-id-on-vercel",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
